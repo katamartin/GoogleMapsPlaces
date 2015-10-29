bd78544e4f8014b7c39cd64e0d321c6c6d577f44
katamartin <katasm@gmail.com>
2015-10-29 13:27:07 -0700

    add api tutorial search and marker placement

--- application.js
+++ application.js
@@ -12,6 +12,58 @@
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(this.setLocation.bind(this));
     }
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    this.searchBox = searchBox;
+
+    this.map.addListener('bounds_changed', function() {
+      searchBox.setBounds(this.map.getBounds());
+    }.bind(this));
+
+    this.markers = [];
+    searchBox.addListener('places_changed', this.updateMarkers.bind(this));
+  };
+
+  Map.updateMarkers = function() {
+    var places = this.searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    this.markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: this.map,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    }.bind(this));
+    this.markers = markers;
+    this.map.fitBounds(bounds);
   };
 
   Map.setLocation = function(position) {
