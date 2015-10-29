b9ebe343ade1eb7386bb3d0a9e50fec01a845649
katamartin <katasm@gmail.com>
2015-10-29 12:41:46 -0700

    use geolocation to center map

--- application.js
+++ application.js
@@ -4,9 +4,20 @@
   }
 
   Map.initMap = function() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    this.center = { lat: 37.7833, lng: -122.4167 };
+    this.map = new google.maps.Map(document.getElementById('map'), {
       zoom: 12,
-      center: { lat: 37.7833, lng: -122.4167 }
+      center: this.center
     });
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.setLocation.bind(this));
+    }
+  };
+
+  Map.setLocation = function(position) {
+    this.center = { lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                  };
+    this.map.setCenter(this.center);
   };
 })();
